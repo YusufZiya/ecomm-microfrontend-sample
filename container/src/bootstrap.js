cdef7354621f2e8ff1e2b8e01edc00bf09b63852
Yusuf Ziya Korol <yusufziyakorol@Yusufs-MacBook-Air.local>
2022-04-28 23:08:07 +0300

    sub-app context

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,14 @@
-import "products/ProductsIndex";
-import "cart/CartShow";
+import { mount } from "products/ProductsIndex";
+import { mount as mountCart } from "cart/CartShow";
 
 console.log("Container!");
+
+const products = document.querySelector("#container-products");
+if (products) {
+  mount(products);
+}
+
+const cart = document.querySelector("#container-cart");
+if (cart) {
+  mountCart(cart);
+}
--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,10 +1,26 @@
 import faker from "faker";
 
-let prodcuts = "";
+const mount = (el) => {
+  let prodcuts = "";
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  prodcuts += `<div>${name}</div>`;
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    prodcuts += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = prodcuts;
+};
+
+//Context/Situation 1
+//Localde calisma durumu
+if (process.env.NODE_ENV === "development") {
+  const el = document.querySelector("#dev-products");
+
+  if (el) {
+    mount(el);
+  }
 }
 
-document.querySelector("#dev-products").innerHTML = prodcuts;
+//Context/Situation 2
+//Container icerisinde calisma durumu
+export { mount };
